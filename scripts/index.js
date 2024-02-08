e7090f68d0a86bcbfc0a79e1814fea6617184d86
Maksim Bykov <yhormstarfall@gmail.com>
2024-02-08 19:48:23 +0300

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,23 @@
 // @todo: Темплейт карточки
-const cardTemplate = document.querySelector('#card-template').content;
-
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
-const placesList = document.querySelector('.places__list');
+const placesList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
-const cardCreate = (cardDetails) => {
-  const cardElement = cardTemplate.querySelector('.card').cloneNode(true); // клонирование теплейта
-  const buttonDelete = cardElement.querySelector('.card__delete-button');
+const cardCreate = (initialCards) => {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true); // клонирование теплейта
+  const buttonDelete = cardElement.querySelector(".card__delete-button");
 
-  cardElement.querySelector('.card__image').src = cardDetails.link;
-  cardElement.querySelector('.card__title').textContent = cardDetails.name;
+  cardElement.querySelector(".card__image").src = initialCards.link;
+  cardElement.querySelector(".card__title").textContent = initialCards.name;
 
-  buttonDelete.addEventListener('click', () => {
+  buttonDelete.addEventListener("click", () => {
     cardRemove(cardElement);
   });
 
   return cardElement;
-}
+};
 
 // @todo: Функция удаления карточки
 function cardRemove(cardElement) {
